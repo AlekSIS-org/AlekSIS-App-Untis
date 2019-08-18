d4d22f2846b4919dd14b0dc6cf39abdec7dbc6a9
Tom Teichler <tom.teichler@teckids.org>
2019-08-18 21:34:17 +0200

    Update translations.

--- biscuit/apps/untis/menus.py
+++ biscuit/apps/untis/menus.py
@@ -1,12 +1,15 @@
+from django.utils.translation import gettext as _
+
 MENUS = {
     'NAV_MENU_CORE': [
         {
-            'name': 'Interfaces',
+            'name': _('Interfaces'),
             'url': '#',
             'root': True,
+            'validators': ['menu_generator.validators.is_authenticated', 'menu_generator.validators.is_superuser'],
             'submenu': [
                 {
-                    'name': 'Untis import',
+                    'name': _('Untis import'),
                     'url': 'untis_import',
                     'validators': ['menu_generator.validators.is_authenticated', 'menu_generator.validators.is_superuser']
                 }
