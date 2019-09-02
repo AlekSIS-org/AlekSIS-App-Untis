debc102a205ed36723fba310fa08e86443a08cd6
Tom Teichler <tom.teichler@teckids.org>
2019-09-02 23:24:57 +0200

    Use validator if user is linked with a person.

--- biscuit/apps/untis/menus.py
+++ biscuit/apps/untis/menus.py
@@ -5,7 +5,7 @@ MENUS = {
         {
             'name': _('Units import'),
             'url': 'untis_import',
-            'validators': ['menu_generator.validators.is_authenticated', 'menu_generator.validators.is_superuser'],
+            'validators': ['menu_generator.validators.is_authenticated', 'menu_generator.validators.is_superuser', 'biscuit.core.util.core_helpers.has_person'],
         }
     ]
 }
