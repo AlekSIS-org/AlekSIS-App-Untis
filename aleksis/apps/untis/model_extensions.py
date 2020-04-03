77732e1e0d752175b6575433118495b0c2bab484
Jonathan Weth <git@jonathanweth.de>
2020-04-03 15:16:24 +0200

    Make import references optional and add verbose names

--- aleksis/apps/untis/model_extensions.py
+++ aleksis/apps/untis/model_extensions.py
@@ -6,8 +6,16 @@ from aleksis.apps.chronos import models as chronos_models
 from aleksis.core import models as core_models
 
 # Core models
-core_models.Person.field(import_ref_untis=IntegerField())
-core_models.Group.field(import_ref_untis=IntegerField())
+core_models.Person.field(
+    import_ref_untis=IntegerField(
+        verbose_name=_("UNTIS import reference"), null=True, blank=True
+    )
+)
+core_models.Group.field(
+    import_ref_untis=IntegerField(
+        verbose_name=_("UNTIS import reference"), null=True, blank=True
+    )
+)
 if config.UNTIS_IMPORT_MYSQL_USE_COURSE_GROUPS:
     core_models.Group.field(
         untis_subject=CharField(
@@ -21,10 +29,36 @@ if config.UNTIS_IMPORT_MYSQL_USE_COURSE_GROUPS:
     )
 
 # Chronos models
-chronos_models.Subject.field(import_ref_untis=IntegerField())
-chronos_models.Room.field(import_ref_untis=IntegerField())
-chronos_models.SupervisionArea.field(import_ref_untis=IntegerField())
-chronos_models.Lesson.field(lesson_id_untis=IntegerField())
-chronos_models.Lesson.field(element_id_untis=IntegerField())
-chronos_models.Lesson.field(term_untis=IntegerField())
-chronos_models.LessonPeriod.field(element_id_untis=IntegerField())
+chronos_models.Subject.field(
+    import_ref_untis=IntegerField(
+        verbose_name=_("UNTIS import reference"), null=True, blank=True
+    )
+)
+chronos_models.Room.field(
+    import_ref_untis=IntegerField(
+        verbose_name=_("UNTIS import reference"), null=True, blank=True
+    )
+)
+chronos_models.SupervisionArea.field(
+    import_ref_untis=IntegerField(
+        verbose_name=_("UNTIS import reference"), null=True, blank=True
+    )
+)
+chronos_models.Lesson.field(
+    lesson_id_untis=IntegerField(
+        verbose_name=_("Lesson id in UNTIS"), null=True, blank=True
+    )
+)
+chronos_models.Lesson.field(
+    element_id_untis=IntegerField(
+        verbose_name=_("Number of lesson element in UNTIS"), null=True, blank=True
+    )
+)
+chronos_models.Lesson.field(
+    term_untis=IntegerField(verbose_name=_("Term id in UNTIS"), null=True, blank=True)
+)
+chronos_models.LessonPeriod.field(
+    element_id_untis=IntegerField(
+        verbose_name=_("Number of lesson element in UNTIS"), null=True, blank=True
+    )
+)
