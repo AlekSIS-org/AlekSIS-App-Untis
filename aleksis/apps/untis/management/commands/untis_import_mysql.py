30f1c6ec7d2890d8cea543a77a25c2cc48386469
Dominik George <dominik.george@teckids.org>
2020-04-18 12:26:23 +0200

    Use celery_optional task from management command as well

--- aleksis/apps/untis/management/commands/untis_import_mysql.py
+++ aleksis/apps/untis/management/commands/untis_import_mysql.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from aleksis.apps.untis.util.mysql.main import untis_import_mysql
+from ...tasks import untis_import_mysql
 
 
 class Command(BaseCommand):
