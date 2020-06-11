14f0af03190e9a979f0a2fdc155b4e455388515a
Jonathan Weth <git@jonathanweth.de>
2020-06-11 17:07:34 +0200

    Remove legacy subject linking stuff

--- aleksis/apps/untis/urls.py
+++ aleksis/apps/untis/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("import/xml/", views.xml_import, name="untis_xml_import"),
-    path("groups_subjects", views.groups_subjects, name="untis_groups_subjects"),
 ]
--- aleksis/apps/untis/views.py
+++ aleksis/apps/untis/views.py
@@ -1,14 +1,10 @@
-from django.contrib import messages
-from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.utils.translation import gettext as _
 
 from rules.contrib.views import permission_required
 
-from aleksis.core.models import Group
 
-from .forms import GroupSubjectFormset, UntisUploadForm
+from .forms import UntisUploadForm
 from .util.xml.xml import untis_import_xml
 
 
@@ -27,32 +23,3 @@ def xml_import(request: HttpRequest) -> HttpResponse:
     context["upload_form"] = upload_form
 
     return render(request, "untis/xml_import.html", context)
-
-
-@permission_required("untis.assign_subjects_to_groups")
-def groups_subjects(request: HttpRequest) -> HttpResponse:
-    """Assign subjects to groups (for matching by MySQL importer)."""
-    context = {}
-
-    groups_qs = Group.objects.all()
-
-    # Paginate
-    paginator = Paginator(groups_qs, 100)
-    page_number = request.GET.get("page")
-    page = paginator.get_page(page_number)
-    groups_paged = groups_qs.filter(id__in=[g.id for g in page])
-
-    # Create filtered queryset
-    group_subject_formset = GroupSubjectFormset(request.POST or None, queryset=groups_paged)
-
-    # Check if form is submitted and valid, then save
-    if request.method == "POST":
-        if group_subject_formset.is_valid():
-            group_subject_formset.save()
-            messages.success(request, _("Your changes were successfully saved."))
-
-    context["formset"] = group_subject_formset
-    context["page"] = page
-    context["paginator"] = paginator
-
-    return render(request, "untis/groups_subjects.html", context)
