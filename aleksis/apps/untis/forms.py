ee024f0537dd4069bfe5b50e8ae421c7f4feba9f
Dominik George <nik@naturalnet.de>
2021-05-20 12:31:46 +0200

    Remove XML importer for now
    This can be reverted completely when working on #7 outside of the release cycle.

--- aleksis/apps/untis/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.utils.translation import gettext_lazy as _
-
-
-class UntisUploadForm(forms.Form):
-    untis_xml = forms.FileField(label=_("Untis XML export"))
